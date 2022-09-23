08cf6e468aaa2777bbf554756be8bc33d66e41b1
ipandl <igorpandl@gmail.com>
2022-09-23 11:45:29 +0200

    dec_coaches: change coaches detail

--- src/store/modules/coaches/index.js
+++ src/store/modules/coaches/index.js
@@ -9,20 +9,20 @@ export default {
       coaches: [
         {
           id: 'c1',
-          firstName: 'Maximilian',
-          lastName: 'Schwarzmüller',
+          firstName: 'Denis',
+          lastName: 'Back',
           areas: ['frontend', 'backend', 'career'],
           description:
-            "I'm Maximilian and I've worked as a freelance web developer for years. Let me help you become a developer as well!",
-          hourlyRate: 30,
+            "I'm Denis and I've worked as a freelance web developer for years",
+          hourlyRate: 35,
         },
         {
           id: 'c2',
           firstName: 'Julie',
-          lastName: 'Jones',
+          lastName: 'Front',
           areas: ['frontend', 'career'],
           description:
-            'I am Julie and as a senior developer in a big tech company, I can help you get your first job or progress in your current role.',
+            'I am Julie and as a senior developer in a big tech company, I can help you progress in your current role',
           hourlyRate: 30,
         },
       ],
